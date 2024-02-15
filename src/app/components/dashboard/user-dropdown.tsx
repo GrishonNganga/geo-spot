4d17de4f9d3f91ebbd5ea1b5d12d4f0366edb4a4
Grishon Nganga <grishon.nganga01@gmail.com>
2024-02-15 12:09:06 +0300

    Add themeToggle feature

--- src/app/components/dashboard/user-dropdown.tsx
+++ src/app/components/dashboard/user-dropdown.tsx
@@ -12,14 +12,39 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { LogOutIcon } from "lucide-react";
 import { signOut } from "next-auth/react";
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "@/components/ui/button";
 
 export default function UserDropDown({ session }: { session: any }) {
+    const { setTheme } = useTheme()
+
     return (
-        <div className="p-2.5">
+        <div className=" flex gap-x-2 p-2.5">
+<DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                        <span className="sr-only">Toggle theme</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                        Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                        System
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Avatar>
-                        <AvatarImage src={session.user.image} alt={`${session.user.name}`}/>
+                        <AvatarImage src={session.user.image} alt={`${session.user.name}`} />
                         <AvatarFallback className="capitalize font-semibold tex-lg cursor-pointer">
                             {session.user.name.split(" ")[0][0]} {session.user.name.split(" ")[1][0]}
                         </AvatarFallback>
