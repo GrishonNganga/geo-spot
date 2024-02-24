388c480eb9f4c2bbbbcaeea78c7953273f353264
Grishon Nganga <grishon.nganga01@gmail.com>
2024-02-24 17:27:25 +0300

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
           <Toaster position="top-right" />
         </ThemeProvider>
       </body>
