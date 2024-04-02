ee87eab63f9e5a1ba8c29dd262e535d2eb98f28d
Grishon Nganga <grishon.nganga01@gmail.com>
2024-04-02 17:59:30 +0300

    Change tree planted location icon to the forest svg

--- src/app/components/group/trees-column.tsx
+++ src/app/components/group/trees-column.tsx
@@ -5,6 +5,7 @@ import {
 import { TreePine } from "lucide-react"
 import { ITreeType, IUser } from "@/lib/types"
 import { Avatar } from "@/components/ui/avatar"
+import Image from "next/image"
 
 export const columns: ColumnDef<ITreeType>[] = [
 
@@ -15,8 +16,8 @@ export const columns: ColumnDef<ITreeType>[] = [
         cell: ({ row }) => (
             <div className="flex gap-x-2 items-center">
                 <Avatar>
-                    <div className="w-full uppercase px-2.5 py-2 bg-green-100 text-xl rounded-md tracking-widest flex items-center justify-center text-green-500">
-                        <TreePine />
+                    <div className="w-full uppercase text-xl rounded-md tracking-widest flex items-center justify-center text-green-500">
+                        <Image src={"/forest.svg"} width={100} height={100} alt="Forest icon" className="w-24"/>
                     </div>
                 </Avatar>
                 <div className="flex flex-col gap-y-2">
